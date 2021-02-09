e2a7f6d882e8ec197e5f2bc46d16e23f41d309a4
xinhong <xinhong9299@gmail.com>
2021-02-09 18:57:00 +0800

    Add menu and product view and product scroll (buggy)

---
+++ js/medicine.js
@@ -0,0 +1,45 @@
+let medicine =
+[
+    {
+        "id":"1",
+        "medicine":"aspirin",
+        "ingredient":["ingredient 1","ingredient 2","ingredient 3"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/aspirin.jpg"
+    },
+    {
+        "id":"2",
+        "medicine":"pill",
+        "ingredient":["ingredient 1","ingredient 2"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/pill.jpg"
+    },
+    {
+        "id":"3",
+        "medicine":"pill2",
+        "ingredient":["ingredient 1","ingredient 2","ingredient 3"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/pill-2.jpg"
+    },
+    {
+        "id":"4",
+        "medicine":"anything",
+        "ingredient":["ingredient 1","ingredient 2"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/anything.jpg"
+    },
+    {
+        "id":"5",
+        "medicine":"fish-oil",
+        "ingredient":["ingredient 1","ingredient 2","ingredient 3"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/fish-oil.jpg"
+    },
+    {
+        "id":"6",
+        "medicine":"anything2",
+        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/anything-2-2.jpg"
+    }
+]
