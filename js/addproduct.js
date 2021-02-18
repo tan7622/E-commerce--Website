bab0c8a86035d0d4963622d5f8bea1f4bc65170f
tan7622 <55863140+tan7622@users.noreply.github.com>
2021-02-18 23:13:18 +0800

    Product details added

--- js/addproduct.js
+++ js/addproduct.js
@@ -29,19 +29,23 @@ defaultBtn.addEventListener("change", function(){
   }
 });
 
-// defaultBtn2.addEventListener("change", function(){
-//     const file = this.files[0];
-//     if(file){
-//       const reader = new FileReader();
-//       reader.onload = function(){
-//         const result = reader.result;
-//         img.src = result;
-//         wrapper.classList.add("active");
-//       }
-//       reader.readAsDataURL(file);
+// defaultBtn.addEventListener("change", function(){
+//   const file = this.files[0];
+//   if(file){
+//     const reader = new FileReader();
+//     reader.onload = function(){
+//       const result = reader.result;
+//       img.src = result;
+//       wrapper.classList.add("active");
 //     }
-//     if(this.value){
-//       let valueStore = this.value.match(regExp);
-//       fileName.textContent = valueStore;
-//     }
-//   });
+//     clearBtn.addEventListener("click", function(){
+//       img.src = "";
+//       wrapper.classList.remove("active");
+//     })
+//     reader.readAsDataURL(file);
+//   }
+//   if(this.value){
+//     let valueStore = this.value.match(regExp);
+//     fileName.textContent = valueStore;
+//   }
+// });
