6d295aa1564da5dc0affac69d83c73af35fdbd3e
xinhong <xinhong9299@gmail.com>
2021-04-07 12:19:37 +0800

    add shoppingCart, trackOrder and edit catalogue

---
+++ js/orderList.js
@@ -0,0 +1,71 @@
+/*
+    received, packed, delivering, complete
+*/
+let orderList = [
+    {
+        'id':'1',
+        'items':[
+            {
+                'id':'2',
+                'quantity':'3'
+            },
+            {
+                'id':'4',
+                'quantity':'3'
+            },
+        ],
+        'status':{
+            'received':{
+                'date':'8 Feb 2021',
+                'time':'8:00AM'
+            },
+            'packed':{
+                'date':'9 Feb 2021',
+                'time':'8:00AM'
+            },
+            'delivering':{
+                'date':'9 Feb 2021',
+                'time':'10:00AM'
+            },
+            'complete':{
+                'date':'9 Feb 2021',
+                'time':'01:00PM'
+            }
+        }
+    },
+    {
+        'id':'2',
+        'items':[
+            {
+                'id':'1',
+                'quantity':'2'
+            },
+            {
+                'id':'4',
+                'quantity':'3'
+            },
+            {
+                'id':'2',
+                'quantity':'1'
+            }
+        ],
+        'status':{
+            'received':{
+                'date':'8 Feb 2021',
+                'time':'8:00AM'
+            },
+            'packed':{
+                'date':'9 Feb 2021',
+                'time':'8:00AM'
+            },
+            'delivering':{
+                'date':'',
+                'time':''
+            },
+            'complete':{
+                'date':'',
+                'time':''
+            }
+        }
+    },
+]
---
+++ js/shoppingList.js
@@ -0,0 +1,31 @@
+let shoppingList =
+[
+    {
+        "id":"1",
+        "quantity":"2"
+    },
+    {
+        "id":"2",
+        "quantity":"2"
+    },
+    {
+        "id":"3",
+        "quantity":"2"
+    },
+    {
+        "id":"4",
+        "quantity":"2"
+    },
+    {
+        "id":"5",
+        "quantity":"2"
+    },
+    {
+        "id":"6",
+        "quantity":"2"
+    },
+    {
+        "id":"7",
+        "quantity":"2"
+    }
+]
