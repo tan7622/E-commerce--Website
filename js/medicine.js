2e8af723f5f235039fc08b42d425c507ea3e71f2
tan7622 <55863140+tan7622@users.noreply.github.com>
2021-04-10 18:17:39 +0800

    something

--- js/medicine.js
+++ js/medicine.js
@@ -53,5 +53,64 @@ let medicine =
         "path":"/res/anything-2-2.jpg",
         "symptom":["symptom 1","symptom 2","symptom 3"],
         "price":"20.99"
+    },
+    {
+        "id":"7",
+        "medicine":"anything3",
+        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/anything-2-2.jpg",
+        "symptom":["symptom 1","symptom 2","symptom 3"],
+        "price":"20.99"
+    },
+    {
+        "id":"8",
+        "medicine":"anything3",
+        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/anything-2-2.jpg",
+        "symptom":["symptom 1","symptom 2","symptom 3"],
+        "price":"20.99"
+    },
+    {
+        "id":"9",
+        "medicine":"anything3",
+        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/anything-2-2.jpg",
+        "symptom":["symptom 1","symptom 2","symptom 3"],
+        "price":"20.99"
+    },
+    {
+        "id":"10",
+        "medicine":"anything3",
+        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/anything-2-2.jpg",
+        "symptom":["symptom 1","symptom 2","symptom 3"],
+        "price":"20.99"
+    },
+    {
+        "id":"11",
+        "medicine":"anything3",
+        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/anything-2-2.jpg",
+        "symptom":["symptom 1","symptom 2","symptom 3"],
+        "price":"20.99"
+    },
+    {
+        "id":"12",
+        "medicine":"anything3",
+        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "path":"/res/anything-2-2.jpg",
+        "symptom":["symptom 1","symptom 2","symptom 3"],
+        "price":"20.99"
     }
+    
+    
+    
+    
+    
 ]
