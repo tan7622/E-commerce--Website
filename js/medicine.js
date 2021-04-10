e7f0282a9eab176d6672dcd4e358b3cf410f857e
tan7622 <55863140+tan7622@users.noreply.github.com>
2021-04-11 03:43:33 +0800

    js changes

--- js/medicine.js
+++ js/medicine.js
@@ -2,111 +2,111 @@ let medicine =
 [
     {
         "id":"1",
-        "medicine":"aspirin",
-        "ingredient":["ingredient 1","ingredient 2","ingredient 3"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Pantoprazole BEXIMCO 40mg",
+        "ingredient":["Mannitol","Sodium ","Crospovidone"],
+        "description":"Is a combination with 2 appropriate antibiotics for the eradication of Helicobacter pylori in patients with peptic ulcers to reduce recurrence of duodenal and gastric ulcers caused by the microorganism, duodenal and gastric ulcers, moderate and severe cases of inflammation of the esophagus.",
         "path":"/res/aspirin.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"20.99"
+        "symptom":["Stomach pain","Nausea","Headache"],
+        "price":"15.99"
     },
     {
         "id":"2",
-        "medicine":"pill",
-        "ingredient":["ingredient 1","ingredient 2"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Bilaxiten 20mg",
+        "ingredient":["Microcrystalline","Sodium","Anhydrous "],
+        "description":"Is a non-sedating, long-acting histamine antagonist with selective peripheral H1 receptor antagonist affinity and no affinity for muscarinic receptors. Bilastine inhibited histamine-induced wheal and flare skin reactions for 24 hours following single doses.",
         "path":"/res/pill.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"20.99"
+        "symptom":["Sneezing","Itchy","Runny"],
+        "price":"14.30"
     },
     {
         "id":"3",
-        "medicine":"pill2",
-        "ingredient":["ingredient 1","ingredient 2","ingredient 3"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Motilium 10mg",
+        "ingredient":["Lactose","Povidone","Hypromellose"],
+        "description":"An antiemetic and a prokinetic medicine. It works by blocking the action of a chemical messenger in the brain which causes the feeling of nausea and vomiting, as well as increasing the movement or contractions of the stomach and intestines, allowing food to move more easily through the stomach.",
         "path":"/res/pill-2.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"20.99"
+        "symptom":["Sleepiness ","Nervousness","Dizziness"],
+        "price":"12.50"
     },
     {
         "id":"4",
-        "medicine":"anything",
-        "ingredient":["ingredient 1","ingredient 2"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Dynamol 500mg",
+        "ingredient":["Paracetamol","Dihydrocodeine"],
+        "description":"Is a combination of medicines that is used for short term relief of pain, inflammation and swelling. It inhibits release of those chemical messengers in the brain that tell us that we have pain.",
         "path":"/res/anything.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"12.69"
+        "symptom":["Nausea","Heartburn","Diarrhea"],
+        "price":"22.50"
     },
     {
         "id":"5",
-        "medicine":"fish-oil",
-        "ingredient":["ingredient 1","ingredient 2","ingredient 3"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Cetirizine hydrochloride 10mg",
+        "ingredient":["Microcrystalline ","Hypromellose","Macrogol"],
+        "description":"Used to temporarily relieve the symptoms of hay fever (allergy to pollen, dust, or other substances in the air) and allergy to other substances (such as dust mites, animal dander, cockroaches, and molds)",
         "path":"/res/fish-oil.jpg",
-        "symptom":["symptom 1","symptom 2"],
+        "symptom":["Headaches","Dry mouth","Dizzy"],
         "price":"20.99"
     },
     {
         "id":"6",
-        "medicine":"anything2",
-        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Betahistine 24mg",
+        "ingredient":["Dihydrochloride","Crospovidone ","Monohydrate",],
+        "description":"This medication is used to treat dizziness (vertigo) in those who have Meniere's disease",
         "path":"/res/anything-2-2.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"20.99"
+        "symptom":["Dizziness ","Tinnitus","Nausea"],
+        "price":"14.40"
     },
     {
         "id":"7",
-        "medicine":"anything3",
-        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Lingkus Lozenges",
+        "ingredient":["Hyssopusofficinalis","Glycyrrhiza ","Alpinia"],
+        "description":" Is a type of medicine that temporarily relieves cough due to minor throat and bronchial irritation associated with the common cold and also temporary relief of occasional minor irritation, pain, sore mouth and sore throat",
         "path":"/res/anything-2-2.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"20.99"
+        "symptom":["Cough ","Pain ","Irritation"],
+        "price":"4.50"
     },
     {
         "id":"8",
-        "medicine":"anything3",
-        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Allerpid(Loratadine and Pseudoephedrine)",
+        "ingredient":["Kaolin","Antacids","Reserpine"],
+        "description":"Allerpid is a combination medicine used to treat sneezing, runny or stuffy nose, sinus pain, itchy or watery eyes or nose, and other symptoms of allergies and the common cold. Allerpid may also be used for purposes not listed in this medication guide.",
         "path":"/res/anything-2-2.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"20.99"
+        "symptom":["Sneezing","Rhinorrhea","Pruritus"],
+        "price":"5.20"
     },
     {
         "id":"9",
-        "medicine":"anything3",
-        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Benztropine",
+        "ingredient":["Starch","Talc ","Microcrystalline "],
+        "description":"Used along with other medications to treat the symptoms of Parkinson's disease (PD; a disorder of the nervous system that causes difficulties with movement, muscle control, and balance) and tremors caused by other medical problems or medications.",
         "path":"/res/anything-2-2.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"20.99"
+        "symptom":["Dry mouth","Constipation","Vomiting"],
+        "price":"21.51"
     },
     {
         "id":"10",
-        "medicine":"anything3",
-        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Eszopiclone",
+        "ingredient":["Lactose","Triacetin","Calcium ",],
+        "description":"Eszopiclone is used to treat insomnia (trouble sleeping). It belongs to the group of medicines called central nervous system (CNS) depressants, which slow down the nervous system. Eszopiclone helps you get to sleep faster and sleep throughout the night.",
         "path":"/res/anything-2-2.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"20.99"
+        "symptom":["Drowsiness","headache","Rash"],
+        "price":"19.17"
     },
     {
         "id":"11",
-        "medicine":"anything3",
-        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Terbinafine",
+        "ingredient":["Microcrystalline ","hydroxypropyl ","Colloidal"],
+        "description":"Terbinafine used to treat fungal infections of the scalp. Terbinafine tablets are used to treat fungal infections of the toenails and fingernails. Terbinafine is in a class of medications called antifungals. It works by stopping the growth of fungi.",
         "path":"/res/anything-2-2.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"20.99"
+        "symptom":["Itching","Indigestion","Restless"],
+        "price":"156.00"
     },
     {
         "id":"12",
-        "medicine":"anything3",
-        "ingredient":["ingredient 1","ingredient 2","ingredient 3","ingredient 4"],
-        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "medicine":"Doxycycline",
+        "ingredient":["Gelatin","Erythrosine ","Quinoline"],
+        "description":" Doxycycline is a tetracycline antibiotic that fights bacteria in the body. Doxycycline is used to treat many different bacterial infections, such as acne, urinary tract infections, intestinal infections, respiratory infections, eye infections, gonorrhea, chlamydia, syphilis, periodontitis (gum disease) and others",
         "path":"/res/anything-2-2.jpg",
-        "symptom":["symptom 1","symptom 2","symptom 3"],
-        "price":"20.99"
+        "symptom":["Headache","Esophagitis","Hives"],
+        "price":"12.90"
     }
     
     
