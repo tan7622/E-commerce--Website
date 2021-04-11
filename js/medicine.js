05a93ad8be158cecc647ba6d478bfaec9372b2f9
tan7622 <55863140+tan7622@users.noreply.github.com>
2021-04-12 04:04:59 +0800

    image

--- js/medicine.js
+++ js/medicine.js
@@ -2,19 +2,19 @@ let medicine =
 [
     {
         "id":"1",
-        "medicine":"Pantoprazole BEXIMCO 40mg",
-        "ingredient":["Mannitol","Sodium ","Crospovidone"],
-        "description":"Is a combination with 2 appropriate antibiotics for the eradication of Helicobacter pylori in patients with peptic ulcers to reduce recurrence of duodenal and gastric ulcers caused by the microorganism, duodenal and gastric ulcers, moderate and severe cases of inflammation of the esophagus.",
+        "medicine":"Aspirin",
+        "ingredient":["Hypromellose","Cellulose ","Triacetin"],
+        "description":" Aspirin is used to reduce fever and relieve mild to moderate pain from conditions such as muscle aches, toothaches, common cold, and headaches. It may also be used to reduce pain and swelling in conditions such as arthritis. Aspirin is known as a salicylate and a nonsteroidal anti-inflammatory drug (NSAID). ",
         "path":"/res/aspirin.jpg",
-        "symptom":["Stomach pain","Nausea","Headache"],
-        "price":"15.99"
+        "symptom":["Drowsiness","Heartburn","Rash"],
+        "price":"27.07"
     },
     {
         "id":"2",
         "medicine":"Bilaxiten 20mg",
         "ingredient":["Microcrystalline","Sodium","Anhydrous "],
         "description":"Is a non-sedating, long-acting histamine antagonist with selective peripheral H1 receptor antagonist affinity and no affinity for muscarinic receptors. Bilastine inhibited histamine-induced wheal and flare skin reactions for 24 hours following single doses.",
-        "path":"/res/pill.jpg",
+        "path":"/res/Bilaxten_20mg_Box_19.png",
         "symptom":["Sneezing","Itchy","Runny"],
         "price":"14.30"
     },
@@ -23,7 +23,7 @@ let medicine =
         "medicine":"Motilium 10mg",
         "ingredient":["Lactose","Povidone","Hypromellose"],
         "description":"An antiemetic and a prokinetic medicine. It works by blocking the action of a chemical messenger in the brain which causes the feeling of nausea and vomiting, as well as increasing the movement or contractions of the stomach and intestines, allowing food to move more easily through the stomach.",
-        "path":"/res/pill-2.jpg",
+        "path":"/res/Motilium.png",
         "symptom":["Sleepiness ","Nervousness","Dizziness"],
         "price":"12.50"
     },
@@ -32,7 +32,7 @@ let medicine =
         "medicine":"Dynamol 500mg",
         "ingredient":["Paracetamol","Dihydrocodeine"],
         "description":"Is a combination of medicines that is used for short term relief of pain, inflammation and swelling. It inhibits release of those chemical messengers in the brain that tell us that we have pain.",
-        "path":"/res/anything.jpg",
+        "path":"/res/Panadol.png",
         "symptom":["Nausea","Heartburn","Diarrhea"],
         "price":"22.50"
     },
@@ -41,7 +41,7 @@ let medicine =
         "medicine":"Cetirizine hydrochloride 10mg",
         "ingredient":["Microcrystalline ","Hypromellose","Macrogol"],
         "description":"Used to temporarily relieve the symptoms of hay fever (allergy to pollen, dust, or other substances in the air) and allergy to other substances (such as dust mites, animal dander, cockroaches, and molds)",
-        "path":"/res/fish-oil.jpg",
+        "path":"/res/CETIRIZINE-10MG.png",
         "symptom":["Headaches","Dry mouth","Dizzy"],
         "price":"20.99"
     },
@@ -50,7 +50,7 @@ let medicine =
         "medicine":"Betahistine 24mg",
         "ingredient":["Dihydrochloride","Crospovidone ","Monohydrate",],
         "description":"This medication is used to treat dizziness (vertigo) in those who have Meniere's disease",
-        "path":"/res/anything-2-2.jpg",
+        "path":"/res/Betahistine.png",
         "symptom":["Dizziness ","Tinnitus","Nausea"],
         "price":"14.40"
     },
@@ -59,25 +59,25 @@ let medicine =
         "medicine":"Strapsiles Lozenges",
         "ingredient":["Hyssopusofficinalis","Glycyrrhiza ","Alpinia"],
         "description":" Is a type of medicine that temporarily relieves cough due to minor throat and bronchial irritation associated with the common cold and also temporary relief of occasional minor irritation, pain, sore mouth and sore throat",
-        "path":"/res/anything-2-2.jpg",
+        "path":"/res/Lozenges.png",
         "symptom":["Cough ","Pain ","Irritation"],
         "price":"4.50"
     },
     {
         "id":"8",
-        "medicine":"Allerpid(Loratadine and Pseudoephedrine)",
-        "ingredient":["Kaolin","Antacids","Reserpine"],
-        "description":"Allerpid is a combination medicine used to treat sneezing, runny or stuffy nose, sinus pain, itchy or watery eyes or nose, and other symptoms of allergies and the common cold. Allerpid may also be used for purposes not listed in this medication guide.",
-        "path":"/res/anything-2-2.jpg",
-        "symptom":["Sneezing","Rhinorrhea","Pruritus"],
-        "price":"5.20"
+        "medicine":"Pantoprazole BEXIMCO 40mg",
+        "ingredient":["Mannitol","Sodium ","Crospovidone"],
+        "description":"Is a combination with 2 appropriate antibiotics for the eradication of Helicobacter pylori in patients with peptic ulcers to reduce recurrence of duodenal and gastric ulcers caused by the microorganism, duodenal and gastric ulcers, moderate and severe cases of inflammation of the esophagus.",
+        "path":"/res/Beximco.png",
+        "symptom":["Stomach pain","Nausea","Headache"],
+        "price":"15.99"
     },
     {
         "id":"9",
         "medicine":"Benztropine 2mg",
         "ingredient":["Starch","Talc ","Microcrystalline "],
         "description":"Used along with other medications to treat the symptoms of Parkinson's disease (PD; a disorder of the nervous system that causes difficulties with movement, muscle control, and balance) and tremors caused by other medical problems or medications.",
-        "path":"/res/anything-2-2.jpg",
+        "path":"/res/benztropine.png",
         "symptom":["Dry mouth","Constipation","Vomiting"],
         "price":"21.51"
     },
@@ -86,7 +86,7 @@ let medicine =
         "medicine":"Eszopiclone",
         "ingredient":["Lactose","Triacetin","Calcium ",],
         "description":"Eszopiclone is used to treat insomnia (trouble sleeping). It belongs to the group of medicines called central nervous system (CNS) depressants, which slow down the nervous system. Eszopiclone helps you get to sleep faster and sleep throughout the night.",
-        "path":"/res/anything-2-2.jpg",
+        "path":"/res/Eszopiclone.png",
         "symptom":["Drowsiness","headache","Rash"],
         "price":"19.17"
     },
@@ -95,7 +95,7 @@ let medicine =
         "medicine":"Terbinafine 250mg",
         "ingredient":["Microcrystalline ","hydroxypropyl ","Colloidal"],
         "description":"Terbinafine used to treat fungal infections of the scalp. Terbinafine tablets are used to treat fungal infections of the toenails and fingernails. Terbinafine is in a class of medications called antifungals. It works by stopping the growth of fungi.",
-        "path":"/res/anything-2-2.jpg",
+        "path":"/res/Terbinafine.png",
         "symptom":["Itching","Indigestion","Restless"],
         "price":"156.00"
     },
@@ -104,7 +104,7 @@ let medicine =
         "medicine":"Doxycycline 100mg",
         "ingredient":["Gelatin","Erythrosine ","Quinoline"],
         "description":" Doxycycline is a tetracycline antibiotic that fights bacteria in the body. Doxycycline is used to treat many different bacterial infections, such as acne, urinary tract infections, intestinal infections, respiratory infections, eye infections, gonorrhea, chlamydia, syphilis, periodontitis (gum disease) and others",
-        "path":"/res/anything-2-2.jpg",
+        "path":"/res/DOXYCYCLINE.png",
         "symptom":["Headache","Esophagitis","Hives"],
         "price":"12.90"
     }
