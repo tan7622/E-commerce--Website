74186b423eb08203ca645fca19525bf5f9004193
xinhong <xinhong9299@gmail.com>
2021-04-12 04:40:32 +0800

    completed trackOrderAdmin

--- js/orderList.js
+++ js/orderList.js
@@ -31,7 +31,11 @@ let orderList = [
                 'date':'9 Feb 2021',
                 'time':'01:00PM'
             }
-        }
+        },
+        'customer':'Adrian',
+        'address1':'34, Someplace Somewhere 8',
+        'address2':'Somehouse Somearea',
+        'address3':'11900 Penang Malaysia'
     },
     {
         'id':'2',
@@ -66,7 +70,11 @@ let orderList = [
                 'date':'',
                 'time':''
             }
-        }
+        },
+        'customer':'Carol',
+        'address1':'34, Someplace Somewhere 8',
+        'address2':'Somehouse Somearea',
+        'address3':'11900 Penang Malaysia'
     },
     {
         'id':'3',
@@ -101,6 +109,45 @@ let orderList = [
                 'date':'',
                 'time':''
             }
-        }
+        },
+        'customer':'Francesca',
+        'address1':'34, Someplace Somewhere 8',
+        'address2':'Somehouse Somearea',
+        'address3':'11900 Penang Malaysia'
+    },
+    {
+        'id':'4',
+        'items':[
+            {
+                'id':'2',
+                'quantity':'2'
+            },
+            {
+                'id':'4',
+                'quantity':'1'
+            }
+        ],
+        'status':{
+            'received':{
+                'date':'9 Feb 2021',
+                'time':'8:00AM'
+            },
+            'packed':{
+                'date':'19 Feb 2021',
+                'time':'8:00AM'
+            },
+            'delivering':{
+                'date':'20 Feb 2021',
+                'time':'11:00AM'
+            },
+            'complete':{
+                'date':'',
+                'time':''
+            }
+        },
+        'customer':'Francesca',
+        'address1':'34, Someplace Somewhere 8',
+        'address2':'Somehouse Somearea',
+        'address3':'11900 Penang Malaysia'
     },
 ]
---
+++ js/particleAdmin.js
@@ -0,0 +1,67 @@
+
+    particlesJS("particles-js",{
+        "particles": {
+            "number": {
+            "value": 120,
+            "density": {
+                "enable": false,
+                "value_area": 0
+            }
+            },
+            "color": {
+            "value": "#eae081"
+            },
+            "shape": {
+            "type": "circle",
+            "polygon": {
+                "nb_sides": 3
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+            },
+            "opacity": {
+            "value": 1,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 1,
+                "opacity_min": 0,
+                "sync": false
+            }
+            },
+            "size": {
+            "value": 5,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 4,
+                "size_min": 0.3,
+                "sync": false
+            }
+            },
+            "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+            },
+            "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 600
+            }
+            }
+        }
+    });
